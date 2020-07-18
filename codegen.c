675ae2dfbe26eedb066f4c673a7f9bd80f3d1f4f
qrrakakh (Hiroaki KURAMAE) <hiroaki.kuramae@gmail.com>
2020-07-19 04:48:51 +0900

    Cosmetic fix: modify indents in switch block.

--- codegen.c
+++ codegen.c
@@ -361,326 +361,326 @@ void Generate(Node *node) {
     case ND_DUMMY:
       return;
     case ND_FUNC:
-    if (!(node->children[0]))
-      return;
-    printf("  .global %.*s\n", node->val, node->name);
-    printf("%.*s:\n", node->val, node->name);
-    current_func = FindFuncByName(node->name, node->val);
+      if (!(node->children[0]))
+        return;
+      printf("  .global %.*s\n", node->val, node->name);
+      printf("%.*s:\n", node->val, node->name);
+      current_func = FindFuncByName(node->name, node->val);
 
-    locals = node->lvars;
-    // allocate stack for local variables
-    printf("  push rbp\n");
-    printf("  mov rbp, rsp\n");
+      locals = node->lvars;
+      // allocate stack for local variables
+      printf("  push rbp\n");
+      printf("  mov rbp, rsp\n");
 
-    // calculate required reserved stack area sizeand offsets for each lvars
-    lvar_area_size = 0;
-    num_lvar = 0;
-    var = locals;
-    while (var->next) {
-      if(var->kind !=SY_VAR) {
+      // calculate required reserved stack area sizeand offsets for each lvars
+      lvar_area_size = 0;
+      num_lvar = 0;
+      var = locals;
+      while (var->next) {
+        if(var->kind !=SY_VAR) {
+          var = var->next;
+          continue;
+        }
+        ++num_lvar;
+        lvar_area_size += GetTypeSize(var->ty);
+        var->offset_bytes = lvar_area_size;
         var = var->next;
-        continue;
       }
-      ++num_lvar;
-      lvar_area_size += GetTypeSize(var->ty);
-      var->offset_bytes = lvar_area_size;
-      var = var->next;
-    }
 
-    //printf("  sub rsp, %d\n", get_num_lvars() * POINTER_SIZE_BYTES);
-    printf("  sub rsp, %d\n", lvar_area_size);
+      //printf("  sub rsp, %d\n", get_num_lvars() * POINTER_SIZE_BYTES);
+      printf("  sub rsp, %d\n", lvar_area_size);
 
-    // copy passed argument values to the local variables
-    var = locals;
-    while(num_lvar > node->num_args) {
-      var=var->next; --num_lvar;
-    }
-
-    for(int i=0;i<node->num_args;++i) {
-      lvar_idx = node->num_args-1-i;
-      switch(GetSizeVar(var->ty)) {
-        case 1:
-        printf("  mov [rbp-%d], %s\n", var->offset_bytes, x86_64_argreg_8bits[lvar_idx]);
-        break;
-        case 2:
-        printf("  mov [rbp-%d], %s\n", var->offset_bytes, x86_64_argreg_16bits[lvar_idx]);
-        break;
-        case 4:
-        printf("  mov [rbp-%d], %s\n", var->offset_bytes, x86_64_argreg_32bits[lvar_idx]);
-        break;
-        case 8:
-        printf("  mov [rbp-%d], %s\n", var->offset_bytes, x86_64_argreg_64bits[lvar_idx]);
-        break;
+      // copy passed argument values to the local variables
+      var = locals;
+      while(num_lvar > node->num_args) {
+        var=var->next; --num_lvar;
       }
-      var = var->next;
-    }
+
+      for(int i=0;i<node->num_args;++i) {
+        lvar_idx = node->num_args-1-i;
+        switch(GetSizeVar(var->ty)) {
+          case 1:
+            printf("  mov [rbp-%d], %s\n", var->offset_bytes, x86_64_argreg_8bits[lvar_idx]);
+            break;
+          case 2:
+            printf("  mov [rbp-%d], %s\n", var->offset_bytes, x86_64_argreg_16bits[lvar_idx]);
+            break;
+          case 4:
+            printf("  mov [rbp-%d], %s\n", var->offset_bytes, x86_64_argreg_32bits[lvar_idx]);
+            break;
+          case 8:
+            printf("  mov [rbp-%d], %s\n", var->offset_bytes, x86_64_argreg_64bits[lvar_idx]);
+            break;
+        }
+        var = var->next;
+      }
 
 
-    Generate(node->children[0]);
+      Generate(node->children[0]);
 
-    printf(".L.ret%.*s:\n", node->val, node->name);
-    current_func = NULL;
-    
-    // Final evaluated value is already stored on rax, which will be returned.
-    // To back to the original address, we fix the rsp
-    printf("  mov rsp, rbp\n");
-    printf("  pop rbp\n");
-    printf("  ret\n");
+      printf(".L.ret%.*s:\n", node->val, node->name);
+      current_func = NULL;
+      
+      // Final evaluated value is already stored on rax, which will be returned.
+      // To back to the original address, we fix the rsp
+      printf("  mov rsp, rbp\n");
+      printf("  pop rbp\n");
+      printf("  ret\n");
 
-    return;
+      return;
 
     case ND_RETURN:
-    if(node->children[0])
-      Generate(node->children[0]);
-    printf("  jmp .L.ret%.*s\n", current_func->symbol->len, current_func->symbol->name);
-    return;
+      if(node->children[0])
+        Generate(node->children[0]);
+      printf("  jmp .L.ret%.*s\n", current_func->symbol->len, current_func->symbol->name);
+      return;
 
     case ND_BREAK:
-    printf("  jmp .L.end%06d\n", ctrl_depth);
-    return;
+      printf("  jmp .L.end%06d\n", ctrl_depth);
+      return;
 
     case ND_CONTINUE:
-    printf("  jmp .L.next%06d\n", ctrl_depth);
-    return;
+      printf("  jmp .L.next%06d\n", ctrl_depth);
+      return;
 
     case ND_BLOCK:
-    stmt_list = node->children;
-    while (*stmt_list != NULL) {
-      Generate(*(stmt_list++));
-    }
-    printf("  mov rax, 0\n");
-    return;
+      stmt_list = node->children;
+      while (*stmt_list != NULL) {
+        Generate(*(stmt_list++));
+      }
+      printf("  mov rax, 0\n");
+      return;
 
     case ND_ADDR:
-    GenLval(node->children[0]);
-    return;
+      GenLval(node->children[0]);
+      return;
 
     case ND_DEREF:
-    Generate(node->children[0]);
-    LoadVar(node->ty);
-    return;
+      Generate(node->children[0]);
+      LoadVar(node->ty);
+      return;
 
     case ND_ARROW:
-    Generate(node->children[0]);
-    printf("  lea rax, [rax+%d]\n",
-      GetStructMemberOffset(node->children[0]->ty->ptr_to->id, node->id));
-    LoadVar(node->ty);
-    return;
+      Generate(node->children[0]);
+      printf("  lea rax, [rax+%d]\n",
+        GetStructMemberOffset(node->children[0]->ty->ptr_to->id, node->id));
+      LoadVar(node->ty);
+      return;
 
     case ND_DOT:
-    GenLval(node->children[0]);
-    printf("  lea rax, [rax+%d]\n",
-      GetStructMemberOffset(node->children[0]->ty->id, node->id));
-    LoadVar(node->ty);
-    return;
+      GenLval(node->children[0]);
+      printf("  lea rax, [rax+%d]\n",
+        GetStructMemberOffset(node->children[0]->ty->id, node->id));
+      LoadVar(node->ty);
+      return;
 
     case ND_NUM:
     case ND_CHAR:
-    printf("  mov rax, %d\n", node->val);
-    return;
+      printf("  mov rax, %d\n", node->val);
+      return;
 
     case ND_STRINGS:
-    printf("  lea rax, .LC%06d[rip]\n", node->id);
-    return;
+      printf("  lea rax, .LC%06d[rip]\n", node->id);
+      return;
 
     case ND_SIZEOF:
-    // TODO: when we implement struct, judge if the var is struct or not
-    printf("  mov rax,%d\n", GetTypeSize(node->children[0]->ty));
-    return;
+      // TODO: when we implement struct, judge if the var is struct or not
+      printf("  mov rax,%d\n", GetTypeSize(node->children[0]->ty));
+      return;
 
     case ND_NOT:
-    Generate(node->children[0]);
-    printf("  not eax\n");
-    return;
+      Generate(node->children[0]);
+      printf("  not eax\n");
+      return;
 
     case ND_PREINC:
-    diff = 1;
-    if(node->children[0]->ty->kind == TYPE_PTR) {
-      diff = GetSizePtrTarget(node->children[0]->ty);
-    }
-    GenLval(node->children[0]);
-    printf("  push rax\n");
-    LoadVar(node->children[0]->ty);
-    printf("  add rax, %d\n", diff);
-    printf("  push rax\n");
-    StoreVar(node->children[0]->ty, 0, 1);
-    return;
+      diff = 1;
+      if(node->children[0]->ty->kind == TYPE_PTR) {
+        diff = GetSizePtrTarget(node->children[0]->ty);
+      }
+      GenLval(node->children[0]);
+      printf("  push rax\n");
+      LoadVar(node->children[0]->ty);
+      printf("  add rax, %d\n", diff);
+      printf("  push rax\n");
+      StoreVar(node->children[0]->ty, 0, 1);
+      return;
 
     case ND_PREDEC:
-    diff = 1;
-    if(node->children[0]->ty->kind == TYPE_PTR) {
-      diff = GetSizePtrTarget(node->children[0]->ty);
-    }
-    GenLval(node->children[0]);
-    printf("  push rax\n");
-    LoadVar(node->children[0]->ty);
-    printf("  sub rax, %d\n", diff);
-    printf("  push rax\n");
-    StoreVar(node->children[0]->ty, 0, 1);
-    return;
+      diff = 1;
+      if(node->children[0]->ty->kind == TYPE_PTR) {
+        diff = GetSizePtrTarget(node->children[0]->ty);
+      }
+      GenLval(node->children[0]);
+      printf("  push rax\n");
+      LoadVar(node->children[0]->ty);
+      printf("  sub rax, %d\n", diff);
+      printf("  push rax\n");
+      StoreVar(node->children[0]->ty, 0, 1);
+      return;
 
     case ND_POSTINC:
-    diff = 1;
-    if(node->children[0]->ty->kind == TYPE_PTR) {
-      diff = GetSizePtrTarget(node->children[0]->ty);
-    }
-    GenLval(node->children[0]);
-    printf("  push rax\n");
-    LoadVar(node->children[0]->ty);
-    printf("  mov rsi, rax\n");
-    printf("  add rax, %d\n", diff);
-    printf("  push rax\n");
-    StoreVar(node->children[0]->ty, 0, 0);
-    printf("  mov rax, rsi\n");
-    return;
+      diff = 1;
+      if(node->children[0]->ty->kind == TYPE_PTR) {
+        diff = GetSizePtrTarget(node->children[0]->ty);
+      }
+      GenLval(node->children[0]);
+      printf("  push rax\n");
+      LoadVar(node->children[0]->ty);
+      printf("  mov rsi, rax\n");
+      printf("  add rax, %d\n", diff);
+      printf("  push rax\n");
+      StoreVar(node->children[0]->ty, 0, 0);
+      printf("  mov rax, rsi\n");
+      return;
 
     case ND_POSTDEC:
-    diff = 1;
-    if(node->children[0]->ty->kind == TYPE_PTR) {
-      diff = GetSizePtrTarget(node->children[0]->ty);
-    }
-    GenLval(node->children[0]);
-    printf("  push rax\n");
-    LoadVar(node->children[0]->ty);
-    printf("  mov rsi, rax\n");
-    printf("  sub rax, %d\n", diff);
-    printf("  push rax\n");
-    StoreVar(node->children[0]->ty, 0, 0);
-    printf("  mov rax, rsi\n");
-    return;
+      diff = 1;
+      if(node->children[0]->ty->kind == TYPE_PTR) {
+        diff = GetSizePtrTarget(node->children[0]->ty);
+      }
+      GenLval(node->children[0]);
+      printf("  push rax\n");
+      LoadVar(node->children[0]->ty);
+      printf("  mov rsi, rax\n");
+      printf("  sub rax, %d\n", diff);
+      printf("  push rax\n");
+      StoreVar(node->children[0]->ty, 0, 0);
+      printf("  mov rax, rsi\n");
+      return;
 
     case ND_LIST:
-    for(int i=0;i<node->num_args;++i) {
-      Generate(node->children[i]);
-    }
-    return;
+      for(int i=0;i<node->num_args;++i) {
+        Generate(node->children[i]);
+      }
+      return;
 
     case ND_GVAR:
     case ND_LVAR:
-    GenLval(node);
-    LoadVar(node->ty);
-    return;
+      GenLval(node);
+      LoadVar(node->ty);
+      return;
 
     case ND_INIT:
-    GenLval(node);
-    InitLvar(node, 0, 1);
-    return;
+      GenLval(node);
+      InitLvar(node, 0, 1);
+      return;
 
     case ND_ASSIGN:
-    GenLval(node->children[0]);
-    printf("  push rax\n");
-    Generate(node->children[1]);
-    printf("  push rax\n");
-    StoreVar(node->ty, 0, 1);
-    return;
+      GenLval(node->children[0]);
+      printf("  push rax\n");
+      Generate(node->children[1]);
+      printf("  push rax\n");
+      StoreVar(node->ty, 0, 1);
+      return;
 
     case ND_WHILE:
-    label = label_index++;
-    prev_ctrl_depth = ctrl_depth;
-    ctrl_depth = label;
-    printf(".L.next%06d:\n", label);
-    Generate(node->children[0]);
-    printf("  cmp rax, 0\n");
-    printf("  je .L.end%06d\n", label);
-    Generate(node->children[1]);
-    printf("  jmp .L.next%06d\n", label);
-    printf(".L.end%06d:\n", label);
-    ctrl_depth = prev_ctrl_depth;
-    return;
+      label = label_index++;
+      prev_ctrl_depth = ctrl_depth;
+      ctrl_depth = label;
+      printf(".L.next%06d:\n", label);
+      Generate(node->children[0]);
+      printf("  cmp rax, 0\n");
+      printf("  je .L.end%06d\n", label);
+      Generate(node->children[1]);
+      printf("  jmp .L.next%06d\n", label);
+      printf(".L.end%06d:\n", label);
+      ctrl_depth = prev_ctrl_depth;
+      return;
 
     case ND_FOR:
-    label = label_index++;
-    prev_ctrl_depth = ctrl_depth;
-    ctrl_depth = label;
-    Generate(node->children[0]);
-    printf(".L.begin%06d:\n", label);
-    Generate(node->children[1]);
-    printf("  cmp rax, 0\n");
-    printf("  je .L.end%06d\n", label);
-    Generate(node->children[3]);
-    printf(".L.next%06d:\n", label);
-    Generate(node->children[2]);
-    printf("  jmp .L.begin%06d\n", label);
-    printf(".L.end%06d:\n", label);
-    ctrl_depth = prev_ctrl_depth;
-    return;
+      label = label_index++;
+      prev_ctrl_depth = ctrl_depth;
+      ctrl_depth = label;
+      Generate(node->children[0]);
+      printf(".L.begin%06d:\n", label);
+      Generate(node->children[1]);
+      printf("  cmp rax, 0\n");
+      printf("  je .L.end%06d\n", label);
+      Generate(node->children[3]);
+      printf(".L.next%06d:\n", label);
+      Generate(node->children[2]);
+      printf("  jmp .L.begin%06d\n", label);
+      printf(".L.end%06d:\n", label);
+      ctrl_depth = prev_ctrl_depth;
+      return;
 
     case ND_SWITCH:
-    label = label_index++;
-    prev_ctrl_depth = ctrl_depth;
-    ctrl_depth = label;
-    Generate(node->children[0]);
-    printf("  mov r10, rax\n");
-    node_cur = node->children[1];
-    while(node_cur) {
-      printf("  cmp r10, %d\n", node_cur->children[0]->val);
-      printf("  je .L.sw%06d.case%06d\n", ctrl_depth, node_cur->val);
-      node_cur = node_cur->children[1];
-    }
-    if(node->num_args==0) { // default
-      printf("  jmp .L.sw%06d.case%06d\n", ctrl_depth, 0);
-    } else {
-      printf("  jmp .L.end%06d\n", label);
-    }
-    Generate(node->children[2]);
-    printf(".L.end%06d:\n", label);
-    ctrl_depth = prev_ctrl_depth;
-    return;
+      label = label_index++;
+      prev_ctrl_depth = ctrl_depth;
+      ctrl_depth = label;
+      Generate(node->children[0]);
+      printf("  mov r10, rax\n");
+      node_cur = node->children[1];
+      while(node_cur) {
+        printf("  cmp r10, %d\n", node_cur->children[0]->val);
+        printf("  je .L.sw%06d.case%06d\n", ctrl_depth, node_cur->val);
+        node_cur = node_cur->children[1];
+      }
+      if(node->num_args==0) { // default
+        printf("  jmp .L.sw%06d.case%06d\n", ctrl_depth, 0);
+      } else {
+        printf("  jmp .L.end%06d\n", label);
+      }
+      Generate(node->children[2]);
+      printf(".L.end%06d:\n", label);
+      ctrl_depth = prev_ctrl_depth;
+      return;
 
     case ND_SWLABEL:
-    printf(".L.sw%06d.case%06d:", ctrl_depth, node->val);
-    return;
+      printf(".L.sw%06d.case%06d:", ctrl_depth, node->val);
+      return;
 
     case ND_IF:
-    label = label_index++;
-    Generate(node->children[0]);
-    printf("  cmp rax, 0\n"); // 0 if cond is not satisfied
-    printf("  je .L.else%06d\n", label);
-    Generate(node->children[1]);
-    printf("  jmp .L.end%06d\n",  label);
-    printf(".L.else%06d:\n",  label);
-    if(node->children[2]!=NULL) {
-      Generate(node->children[2]);
-    }
-    printf(".L.end%06d:\n",  label);
-    return;
+      label = label_index++;
+      Generate(node->children[0]);
+      printf("  cmp rax, 0\n"); // 0 if cond is not satisfied
+      printf("  je .L.else%06d\n", label);
+      Generate(node->children[1]);
+      printf("  jmp .L.end%06d\n",  label);
+      printf(".L.else%06d:\n",  label);
+      if(node->children[2]!=NULL) {
+        Generate(node->children[2]);
+      }
+      printf(".L.end%06d:\n",  label);
+      return;
 
     case ND_CALL:
-    for(int i=0;i<node->num_args;++i) {
-      Generate(node->children[i]);
-      switch(GetSizeVar(node->children[i]->ty)) {
-        case 1:
-        printf("  mov %s, al\n", x86_64_argreg_8bits[i]);
-        break;  
-        case 2:
-        printf("  mov %s, ax\n", x86_64_argreg_16bits[i]);
-        break;  
-        case 4:
-        printf("  mov %s, eax\n", x86_64_argreg_32bits[i]);
-        break;  
-        case 8:
-        printf("  mov %s, rax\n", x86_64_argreg_64bits[i]);
-        break;  
+      for(int i=0;i<node->num_args;++i) {
+        Generate(node->children[i]);
+        switch(GetSizeVar(node->children[i]->ty)) {
+          case 1:
+            printf("  mov %s, al\n", x86_64_argreg_8bits[i]);
+            break;  
+          case 2:
+            printf("  mov %s, ax\n", x86_64_argreg_16bits[i]);
+            break;  
+          case 4:
+            printf("  mov %s, eax\n", x86_64_argreg_32bits[i]);
+            break;  
+          case 8:
+            printf("  mov %s, rax\n", x86_64_argreg_64bits[i]);
+            break;  
+        }
       }
-    }
 
-    // modify rsp as a multiply of 16
-    label = label_index++;
-    printf("  mov rax, rsp\n");
-    printf("  and rax, 15\n");
-    printf("  jnz .L.call%06d\n", label);
-    printf("  mov rax, 0\n");
-    printf("  mov r10, qword ptr %.*s@GOTPCREL[rip]\n", node->val, node->name);
-    printf("  call r10\n");
-    printf("  jmp .L.end%06d\n", label);
-    printf(".L.call%06d:\n", label);
-    printf("  sub rsp, 8\n");
-    printf("  mov rax, 0\n");
-    printf("  mov r10, qword ptr %.*s@GOTPCREL[rip]\n", node->val, node->name);
-    printf("  call r10\n");
-    printf("  add rsp, 8\n");
-    printf(".L.end%06d:\n", label);
-    return;
+      // modify rsp as a multiply of 16
+      label = label_index++;
+      printf("  mov rax, rsp\n");
+      printf("  and rax, 15\n");
+      printf("  jnz .L.call%06d\n", label);
+      printf("  mov rax, 0\n");
+      printf("  mov r10, qword ptr %.*s@GOTPCREL[rip]\n", node->val, node->name);
+      printf("  call r10\n");
+      printf("  jmp .L.end%06d\n", label);
+      printf(".L.call%06d:\n", label);
+      printf("  sub rsp, 8\n");
+      printf("  mov rax, 0\n");
+      printf("  mov r10, qword ptr %.*s@GOTPCREL[rip]\n", node->val, node->name);
+      printf("  call r10\n");
+      printf("  add rsp, 8\n");
+      printf(".L.end%06d:\n", label);
+      return;
   }
 
   Generate(node->children[1]);
